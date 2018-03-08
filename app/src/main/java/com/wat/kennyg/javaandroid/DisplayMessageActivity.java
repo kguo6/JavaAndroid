2ed92c0aa561312bb42b725f8f3ad711eefaa2b1
Kenny <kguo6@my.bcit.ca>
2018-03-08 15:53:47 -0800

    Added just for fun counter

--- app/src/main/java/com/wat/kennyg/javaandroid/DisplayMessageActivity.java
+++ app/src/main/java/com/wat/kennyg/javaandroid/DisplayMessageActivity.java
@@ -3,9 +3,13 @@ package com.wat.kennyg.javaandroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+    private Integer counter = 0;
+    private String numberDisplay;
+    private TextView textView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,13 @@ public class DisplayMessageActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
+
+    }
+
+    public void count(View view){
+        counter = counter+1;
+        numberDisplay = counter.toString();
+        textView3 = findViewById(R.id.textView3);
+        textView3.setText(numberDisplay);
     }
 }
